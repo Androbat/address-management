da6f9736dcf0756f96ff83849d85f977d36601cc
Manuel Reyes <mreyes@prodoctivity.app>
2025-03-03 13:33:30 -0600

    updated class based repository

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,25 +1,14 @@
 import { MongoClient } from 'mongodb';
-import { tryCatch } from 'fp-ts/lib/TaskEither';
-import { MongoConfig } from '../contracts/db.config';
+
 import { mongoEnv, databaseEnv } from './dotenv.conf';
 
 
 
-async function connectMongodb(client: MongoClient, db: string): MongoConfig {
-    return tryCatch(
-        async () => {
-            const connected = await client.connect();
-            if (!connected){
-                console.error(`Unable to connect to mongodb`);
-            } else {
-                console.log(`Successfully connected to database`);
-            }
-            return client.db(db);
-        },
-        (error: unknown) => `Error connecting to MongoDB: ${error}`);
+async function connectMongodb(connectMongo: (db: string) => MongoClient, db: string) {
+    const connect = connectMongo(db);
+    
+
+
 }
 
-
-
-connectMongodb(new MongoClient(mongoEnv), databaseEnv).catch((error) => console.log(error));
 
